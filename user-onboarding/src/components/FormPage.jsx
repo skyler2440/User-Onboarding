3ae504a50b597e9f968e9dda66d2e94fac524daf
skyler <skylerwebdev@gmail.com>
2019-08-07 15:37:01 -0400

    forrm done with stretch and style

--- user-onboarding/src/components/FormPage.jsx
+++ user-onboarding/src/components/FormPage.jsx
@@ -2,38 +2,51 @@ import React from "react";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
-
+import './Form.css'
 function FormPage({ values, errors, touched }) {
   return (
     <>
       <h1>Please Create an Account</h1>
-      <Form>
-        <div>
-          {touched.name && errors.name && <p>{errors.name}</p>}
-          <Field type="text" name="name" placeholder="Name" />
+      <Form className='form'>
+        <div className='form-field-area'>
+          {touched.name && errors.name && <p className='form-error'>{errors.name}</p>}
+          <Field className='form-field'  type="text" name="name" placeholder="Name" />
         </div>
-        <div>
-          {touched.email && errors.email && <p>{errors.email}</p>}
-          <Field type="email" name="email" placeholder="Email" />
+        <div className='form-field-area'>
+          {touched.email && errors.email && <p className='form-error'>{errors.email}</p>}
+          <Field className='form-field'  type="email" name="email" placeholder="Email" />
         </div>
-        <div>
-          {touched.password && errors.password && <p>{errors.password}</p>}
-          <Field type="password" name="password" placeholder="Password" />
+        <div className='form-field-area'>
+          {touched.phone && errors.phone && <p className='form-error'>{errors.phone}</p>}
+          <Field className='form-field'  type="phone" name="phone" placeholder="Phone" />
         </div>
-        <div>
+        <div className='form-field-area'>
+          {touched.password && errors.password && <p className='form-error'>{errors.password}</p>}
+          <Field className='form-field'  type="password" name="password" placeholder="Password" />
+        </div>
+        <div className='form-field-area'>
           {touched.confirmPassword && errors.confirmPassword && (
-            <p>{errors.confirmPassword}</p>
+            <p className='form-error'>{errors.confirmPassword}</p>
           )}
-          <Field
+          <Field className='form-field' 
             type="password"
             name="confirmPassword"
             placeholder="Confirm Password"
           />
         </div>
-        <div>
-        <label htmlFor="tos">
-            {touched.tos && errors.tos && <p>{errors.tos}</p>}
-            <Field type="checkbox" name="tos" checked={values.tos} />
+        <div className='form-field-area'>
+          {touched.role && errors.role && <p className='form-error'>{errors.role}</p>}
+          <Field className='form-field'  component="select" name="role">
+            <option>Pleae Select your Role</option>
+            <option value="front end dev">Front End Developer</option>
+            <option value="back end dev">Back End Developer</option>
+            <option value="full stack dev">Full Stack Developer</option>
+          </Field>
+        </div>
+        <div className='form-field-area'>
+            <label className='tos'   htmlFor="tos">
+            {touched.tos && errors.tos && <p className='form-error'>{errors.tos}</p>}
+            <Field className='tos'  type="checkbox" name="tos" checked={values.tos} />
             Accept TOS
           </label>
         </div>
@@ -44,19 +57,32 @@ function FormPage({ values, errors, touched }) {
 }
 
 const FormikFormPage = withFormik({
-  mapPropsToValues({ name, email, password, confirmPassword, tos }) {
+  mapPropsToValues({
+    name,
+    email,
+    password,
+    confirmPassword,
+    tos,
+    role,
+    phone
+  }) {
     return {
+      phone: phone || "",
       name: name || "",
       email: email || "",
       password: password || "",
       confirmPassword: confirmPassword || "",
-      tos: tos || false
+      tos: tos || false,
+      role: role || ""
     };
   },
   validationSchema: Yup.object().shape({
     name: Yup.string()
-    .required("Name is Required")
-    .max(50, 'Your name is too long'),
+      .required("Name is Required")
+      .max(50, "Your name is too long"),
+    phone: Yup.string()
+      .required("Phone is Required")
+      .min(10, "Phone minimum Length is 10 digits"),
     email: Yup.string()
       .email("Email is not Valid")
       .required("Email is Required"),
@@ -70,14 +96,20 @@ const FormikFormPage = withFormik({
   }),
 
   handleSubmit(values, formikBag) {
-      if (values.email === "waffles@syrup.com") {
-          formikBag.setErrors({email: "That email is already taken"})
-        } else if (values.tos === false) {
-            formikBag.setErrors({tos: "Please Accept the Terms of Service"})
-        }else {
-            console.log(values);
-        }
-        
+    if (values.email === "waffles@syrup.com") {
+      formikBag.setErrors({ email: "That email is already taken" });
+    } else if (values.tos === false) {
+      formikBag.setErrors({ tos: "Please Accept the Terms of Service" });
+    } else if (values.role === "Please Select your Role") {
+      formikBag.setErrors({ role: "Role is Required" });
+    } else if (values.role === "") {
+      formikBag.setErrors({ role: "Role is Required" });
+    } else {
+      axios
+        .post("https://reqres.in/api/users", values)
+        .then(res => console.log(res))
+        .catch(err => console.log(err.response));
+    }
   }
 })(FormPage);
 export default FormikFormPage;
