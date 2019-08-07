81353a0d7b2bc172ced0ee4e3a9d8f77181dce5c
skyler <skylerwebdev@gmail.com>
2019-08-07 11:45:54 -0400

    app setup

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FormPage from './components/FormPage'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<FormPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ user-onboarding/src/components/FormPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { withFormik, Form, field } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+
+function FormPage ({}) {
+    return(
+        <h1>Form Page Working</h1>
+
+    )
+}
+
+export default FormPage;
